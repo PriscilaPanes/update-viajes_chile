f5e8b8c89e3dfb0140fa3498fb6e4e78aca23edd
Priscila Panes <priscilapanestoledo@gmail.com>
2024-03-25 12:23:59 -0300

    se agregan nuevos cambios al archivo, se agregan funciones de tooltips, alerts y modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,22 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 const alertList = document.querySelectorAll('.alert')
 const alerts = [...alertList].map(element => new bootstrap.Alert(element))
 
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close " data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('¡Su mensaje fue enviado con exito!', 'success')
+  })
+}
