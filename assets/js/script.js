d854b2f29db0f49429c6959aed89b1eb53543707
Priscila Panes <priscilapanestoledo@gmail.com>
2024-04-16 11:44:50 -0400

    se modifican algunas cosas

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,8 @@ const appendAlert = (message, type) => {
     '   <button type="button" class="btn-close " data-bs-dismiss="alert" aria-label="Close"></button>',
     '</div>'
   ].join('')
-
   alertPlaceholder.append(wrapper)
 }
-
 const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
   alertTrigger.addEventListener('click', () => {
